708ab05e29c2244b2090230b37dd4dc6784f79de
Zoe Abrams <zoej.abrams@gmail.com>
2020-11-12 11:54:05 -0800

    Add tests

--- src/edu/mills/cs180/PalindromeCheckerTest.java
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PalindromeCheckerTest {
 
@@ -24,4 +25,23 @@ class PalindromeCheckerTest {
   void isPalindrome_False_ValidString(String palindrome) {
     assertFalse(PalindromeChecker.isPalindrome(palindrome));
   }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"Ama", "Wow", "reddER"})
+  void isPalindrome_True_DifferingCase(String s) {
+    assertTrue(PalindromeChecker.isPalindrome(s));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"my gym", "top spot", "step on no pets"})
+  void isPalindrome_True_HavingSpaces(String s) {
+    assertTrue(PalindromeChecker.isPalindrome(s));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"WOW!!!", "I did, did I?", "?!"})
+  void isPalindrome_True_HavingPunctuationAndSpaces(String s) {
+    assertTrue(PalindromeChecker.isPalindrome(s));
+  }
+
 }
