990ca436cd5642897fe33f1484cbaf5bf20bbc52
OBNA <obna.adeleke@gmail.com>
2020-12-27 00:30:07 -0600

    Add more tests

--- src/edu/mills/cs180/PalindromeCheckerTest.java
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -1,14 +1,30 @@
 package edu.mills.cs180;
 
 import static edu.mills.cs180.PalindromeChecker.isPalindrome;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-class PalindromeTester {
+class PalindromeCheckerTest {
 
     @Test
     void isPalindrome_True_EmptyString() {
-        assertTrue(isPalindrome("racecar"));
+        assertTrue(isPalindrome(""));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"AMA", "racecar", "X"})
+    void isPalindrome_True_AcceptedValue(String qPalindrome) {
+        assertTrue(isPalindrome(qPalindrome));
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "abc", "spatula"})
+    void isPalindrome_False_RejectedValue(String qPalindrome) {
+        assertFalse(isPalindrome(qPalindrome));
     }
 
 }
