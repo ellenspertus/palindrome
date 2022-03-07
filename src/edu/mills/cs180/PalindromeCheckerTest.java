fa1571b56f4cc9707f62b408dc5720a11e11da48
Vienna Tran <vitran@mills.edu>
2022-03-06 19:18:56 -0800

    Change test class

---
+++ src/edu/mills/cs180/PalindromeCheckerTest.java
@@ -0,0 +1,13 @@
+package edu.mills.cs180;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.*;
+
+class PalindromeCheckerTest {
+
+    @Test
+    void isPalindrome_True_EmptyString() {
+        fail("Not yet implemented");
+    }
+
+}
